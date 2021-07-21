b420adcc2882b29ddbd1d44f0a46fc408449f0d2
Juan Gomez <juanpg9656@gmail.com>
2021-07-21 10:30:52 -0500

    added camera init, working ok

--- nrf24-esp32-cam-reliable-datagram-client/nrf24-esp32-cam-reliable-datagram-client.ino
+++ nrf24-esp32-cam-reliable-datagram-client/nrf24-esp32-cam-reliable-datagram-client.ino
@@ -2,9 +2,13 @@
 #include <RH_NRF24.h>
 #include <RHSoftwareSPI.h>
 #include <SPI.h>
+#include "esp_camera.h"
+#include "camera_gpio.h"
+
 
 //function declaration
 void init_nrf24();
+void setup_camera();
 
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
@@ -23,6 +27,7 @@ void setup()
   spi.setPins(12, 13, 14);
   Serial.begin(9600);
   init_nrf24();
+  setup_camera();
   
 }
 
@@ -32,6 +37,16 @@ uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
 
 void loop()
 {
+  //take an images
+  camera_fb_t * image = esp_camera_fb_get();
+  if(!image) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+  }
+
+  Serial.println(image->len);
+  
   Serial.println("Sending to nrf24_reliable_datagram_server");
     
   // Send a message to manager_server
@@ -39,7 +54,9 @@ void loop()
   {
     Serial.println("sendtoWait failed");
   }
-    
+
+  esp_camera_fb_return(image);
+  
   delay(500);
 }
 
@@ -54,4 +71,54 @@ void init_nrf24(){
     Serial.println("init succeed");
   }
   // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+}
+
+void setup_camera(){
+
+  //camera configuration variable
+  camera_config_t config; 
+  
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  // init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_CIF;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+  else if(err == ESP_OK){
+    
+    Serial.println("Camera init succeed");
+  }
 }

ae416c55e18398eec6d959c75006a0fc51c65336
Juan Pablo  Gómez García <juanpg9656@gmail.com>
2021-07-21 16:01:23 -0500

    commented and refactor code

--- nrf24-esp8266-reliable-datagram-server/nrf24-esp8266-reliable-datagram-server.ino
+++ nrf24-esp8266-reliable-datagram-server/nrf24-esp8266-reliable-datagram-server.ino
@@ -14,6 +14,24 @@ RH_NRF24 driver(2, 4);
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, SERVER_ADDRESS);
 
+//To compare with the received message, and know if the
+//communication is starting or finishing
+uint8_t start_com[] = "Start";
+uint8_t finish_com[] = "Finish";
+
+/*The buffer length is being send as an array of characters,
+* so first you need to store it in a char array, and then convert 
+* it into an int for further use
+*/
+char char_buffer_length[5];
+int buffer_length = 0;
+
+//to know which pixel data are we storing
+uint16_t counter = 0;
+
+//pointer to the image buffer
+uint8_t * image;
+
 void setup() 
 {
   Serial.begin(115200);
@@ -32,11 +50,60 @@ void loop()
     uint8_t from;
     
     if (manager.recvfromAck(buf, &len, &from)){
+
+      //if the received message is equal to "Finish" then the communication finished
+      if(memcmp(buf, finish_com, 6) == 0){
+        
+        Serial.println((char*)buf);
+
+        for(int i = 0; i < 10; i++){
+         
+          Serial.println(image[i]);
+        }
+        
+        //free the allocated memory for the image buffer
+        free(image);
+
+        //reset the counter for the next communication
+        counter = 0; 
+      }
       
-      Serial.print("got request from : 0x");
-      Serial.print(from, HEX);
-      Serial.print(": ");
-      Serial.println((char*)buf);
+      if(counter > 1){
+
+        //counter 1 is buffer length, counter 2 and so on are pixel data
+        image[0 + (counter - 2)] = buf[0];
+        /*
+        Serial.print("got request from : 0x");
+        Serial.print(from, HEX);
+        Serial.print(": ");
+        Serial.println(buf[0]);
+        */
+        counter++;
+      }
+      
+      //this would be buffer length
+      if(counter == 1){
+
+       //the buffer length comes as an array of characters,
+       //so convert it into an int
+       memcpy(char_buffer_length, buf, 5);
+       buffer_length = atoi(char_buffer_length);       
+
+       //allocate a very big memory for the image buffer
+       image = (uint8_t*)calloc(45000, 1);
+       
+       Serial.println(char_buffer_length);
+       
+       counter++;
+      }
+
+      //if the received message is equal to "Start" then the communication started
+      if(memcmp(buf, start_com, 5) == 0){
+        
+       Serial.println((char*)buf);
+
+       counter++;
+      }
     }
   }
 }
